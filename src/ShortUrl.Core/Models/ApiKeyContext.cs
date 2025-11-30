de50fb41407e066f2dc7bfb7e3b83321b2d12ef2
rogero83 <rogero83@yahoo.it>
2025-11-30 15:14:08 +0100

    Altri commenti mancanti

--- src/ShortUrl.Core/Models/ApiKeyContext.cs
+++ src/ShortUrl.Core/Models/ApiKeyContext.cs
@@ -5,7 +5,18 @@
 /// </summary>
 public class ApiKeyContext
 {
+    /// <summary>
+    /// Api key identifier.
+    /// </summary>
     public long Id { get; set; }
+
+    /// <summary>
+    /// Api key used to authenticate requests to external services.
+    /// </summary>
     public string ApiKey { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Indicating whether custom short codes can be set for this instance.
+    /// </summary>
     public bool CanSetCustomShortCodes { get; set; }
 }
--- src/ShortUrl.Core/Models/ShortCodesResponse.cs
+++ src/ShortUrl.Core/Models/ShortCodesResponse.cs
@@ -5,20 +5,59 @@
 /// </summary>
 public class ShortCodesResponse
 {
+    /// <summary>
+    /// Total number of items available across all pages.
+    /// </summary>
     public int TotalItems { get; set; }
+    /// <summary>
+    /// Actual page number of the current set of results.
+    /// </summary>
     public int Page { get; set; }
+    /// <summary>
+    /// Items included per page in the response.
+    /// </summary>
     public int ItemsByPage { get; set; }
+    /// <summary>
+    /// Results containing the list of short URL information.
+    /// </summary>
     public List<ShortUrlInfo> Items { get; set; } = new();
 }
 
+/// <summary>
+/// Short URL information details.
+/// </summary>
 public record ShortUrlInfo
 {
+    /// <summary>
+    /// Short code representing the shortened URL.
+    /// </summary>
     public string ShortCode { get; set; } = string.Empty;
+    /// <summary>
+    /// Original long URL before shortening.
+    /// </summary>
     public string LongUrl { get; set; } = string.Empty;
+    /// <summary>
+    /// Indicates whether the short URL is currently active.
+    /// </summary>
     public bool IsActive { get; set; }
+    /// <summary>
+    /// Total number of clicks the short URL has received.
+    /// </summary>
     public long TotalClicks { get; set; } = 0;
+    /// <summary>
+    /// Total number of unique clicks the short URL has received.
+    /// </summary>
     public long UniqueClicks { get; set; } = 0;
+    /// <summary>
+    /// Creation date and time of the short URL.
+    /// </summary>
     public DateTime CreatedAt { get; set; }
+    /// <summary>
+    /// Expiration date and time of the short URL, if set; otherwise, null.
+    /// </summary>
     public DateTime? ExpireAt { get; set; }
-    public int Clicks { get; set; }
+    /// <summary>
+    /// Clicks in the last month.
+    /// </summary>
+    public int ClicksInLastMonth { get; set; }
 }
